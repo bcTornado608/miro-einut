8b23b91fd5e40884e6017c991c65138115b77c17
Toby <toby.yang@mail.utoronto.ca>
2022-10-01 14:09:06 -0400

    add main file

---
+++ main.py
@@ -0,0 +1,78 @@
+from miro_lib import Robot
+from state_machine import *
+
+
+
+class StateAlways(State):
+    def on_enter(self):
+        pass
+    def on_exit(self):
+        pass
+    def on_run(self):
+        pass
+
+class StateSleep(State):
+    def on_enter(self):
+        pass
+    def on_exit(self):
+        pass
+    def on_run(self):
+        pass
+
+
+
+class StateLieDown(State):
+    def on_enter(self):
+        pass
+    def on_exit(self):
+        pass
+    def on_run(self):
+        pass
+
+
+
+class StateWake(State):
+    def on_enter(self):
+        pass
+    def on_exit(self):
+        pass
+    def on_run(self):
+        pass
+
+
+
+class StateWondering(State):
+    def on_enter(self):
+        pass
+    def on_exit(self):
+        pass
+    def on_run(self):
+        pass
+
+
+
+class StateCurious(State):
+    def on_enter(self):
+        pass
+    def on_exit(self):
+        pass
+    def on_run(self):
+        pass
+
+
+
+class StateInteractive(State):
+    def on_enter(self):
+        pass
+    def on_exit(self):
+        pass
+    def on_run(self):
+        pass
+
+
+
+
+# main function
+
+if __name__ == '__main__':
+    pass
